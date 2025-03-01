c8a16f2be5acf766aa600278ca2224334ccdf760
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-01 22:05:35 +0530

    first commit

---
+++ ashu/src/main/java/com/Ashutosh/ashu/AshuApplication.java
@@ -0,0 +1,13 @@
+package com.Ashutosh.ashu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshuApplication.class, args);
+	}
+
+}
---
+++ ashu/src/test/java/com/Ashutosh/ashu/AshuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ashutosh.ashu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
